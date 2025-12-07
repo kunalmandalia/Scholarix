8f1d26a9d7e1fafd212d2e943e8e8999cc23edc7
Sahaj Mandalia <sahajmandalia@icloud.com>
2025-12-07 12:57:17 -0500

    buggy code - do not use

---
+++ Scholarix/EditActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  EditActivityView.swift
+//  Scholarix
+//
+//  Created by Kunal Mandalia on 12/7/25.
+//
+
+import SwiftUI
+
+struct EditActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditActivityView()
+}
